b5d10727f9a19c6b4a0e35a1df57e00b73749283
Starpisaurus <edouard.petit.16@gmail.com>
2017-03-08 21:49:08 +0100

    First commit with all HP files

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(
+function() {
+	var vid = document.getElementById("bgvid");
+	var pauseButton = document.querySelector("#pbutton");
+	var vidDiv = document.getElementById("bgviddiv");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vidDiv.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vidDiv.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+});
